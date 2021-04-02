8879caa12183e9d90e307d0caeccb8099f8ec6a1
Andrey <66561503+zetonenine@users.noreply.github.com>
2021-04-03 01:33:03 +0300

    Change Modal.js and Modal.css

--- src/components/ContentBlock/Features/Modal.js
+++ src/components/ContentBlock/Features/Modal.js
@@ -7,11 +7,21 @@ const Modal = ({active, setActive, status}) => {
         <div className={active ? "modal active" : "modal"} onClick={() => setActive(false)}>
             <div className={active ? "modal__content active" : "modal__content"} onClick={e => e.stopPropagation()}>
                 <div className='modal_header'>
+                    {/* <div className='test'>
+
+                    </div> */}
                     <p className='title'> {active['fname']} {active['name']} </p> 
+                </div>
+                
+                <div >
+                    <input className='inputs' value={active['fname']} readOnly></input>
+                    <input className='inputs' value={active['name']} readOnly></input>
+                    <input className='inputs' value={active['mname']} readOnly></input>
                 </div>
                 <span>{active['fname']}</span>
                 <span>{active['name']}</span>  
                 <span>{active['mname']}</span>  
+                
 
                 <span>{status[active['status']]}</span> 
             </div>

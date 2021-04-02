90873d75bb7ab8cb5d812e485cf53eba33b31d62
Andrey <66561503+zetonenine@users.noreply.github.com>
2021-04-02 22:02:20 +0300

    Fixed problem with closing Dropdown

--- src/components/ContentBlock/Features/Dropdown.js
+++ src/components/ContentBlock/Features/Dropdown.js
@@ -3,9 +3,14 @@ import './Dropdown.css';
 import DropdownContent from './DropdownContent.js';
 
 export default function Dropdown(props) {
-   
+    
+    const handleClick = (e) => {
+       e.stopPropagation();
+       props.setActive(false)
+    }
+
     return (
-        <div className={props.active ? 'drop active' : 'drop'} onClick={() => props.setActive(false)}>
+        <div className={props.active ? 'drop active' : 'drop'} onClick={(e) => handleClick(e)}>
             <div className='drop_content' >
                 <DropdownContent user={props.active} setActive={props.setActive}/>
             </div>

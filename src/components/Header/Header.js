e26343c6e5547a6296590fc7e3a8368aa62af30e
Andrey <66561503+zetonenine@users.noreply.github.com>
2021-04-03 15:29:24 +0300

    Made style for Header, but change NAV  and A tags on DIV and P, Links don't work. Commented NAV, A  tags

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -4,13 +4,42 @@ import './Header.css';
 export default function Header() {
     return (
         
-        <header className='border'>
-            <nav>
-                <a href='/'>Все</a>
-                <a href='/blocked'>Заблокированные</a>
-                <a href='/active'>Активные</a>
-            </nav> 
+        <header className='titles'>
+            
+            <div>
+                <div className='text' href='/'>Все
+                    <div className="square"/>
+                    
+                </div>
+            </div>
+            <div>
+                <a className='text' onClick='/blocked'>Заблокированные
+                    <div className="square"/>
+                    
+                </a>
+            </div>
+            <div>
+                <p className='text' href='/active'>Активные
+                    <div className="square"/>
+                </p>
+            </div>
+            
+             
         </header>
     )
 };
+
+
+            // <nav className='nav'>
+
+            //     <a className='a' href='/'>Все</a>
+            //         <div className="square"/>
+                    
+            //     <a className='a' href='/blocked'>Заблокированные</a>
+            //         <div className="square"/>
+                
+            //     <a className='a' href='/active'>Активные</a>
+            //         <div className="square"/>
+                
+            // </nav>
 

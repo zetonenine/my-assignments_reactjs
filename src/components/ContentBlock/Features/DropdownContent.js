384f5a0902ceba1ad115dc42591e6023c6ab92bd
Andrey <66561503+zetonenine@users.noreply.github.com>
2021-04-03 17:47:03 +0300

    Stylized dropdown form

--- src/components/ContentBlock/Features/DropdownContent.js
+++ src/components/ContentBlock/Features/DropdownContent.js
@@ -10,12 +10,10 @@ export default function DropdownContent(props) {
     }
 
     return (
-        <div>
-            <div>
-                <span className={props.user.status === 0 ? '' :'not_chosen_text'} onClick={(e) => handleClick(e, 0, props.user, props.setActive)}>   Приостановлена<br/></span>
-                <span className={props.user.status === 1 ? '' :'not_chosen_text'} onClick={(e) => handleClick(e, 1, props.user, props.setActive)}>   Подписка активна<br/></span>
-                <span className={props.user.status === 2 ? '' :'not_chosen_text'} onClick={(e) => handleClick(e, 2, props.user, props.setActive)}>   Заблокирован</span>
-            </div> 
+        <div className='dropdown_opitons'>
+            <span className={props.user.status === 0 ? '' :'not_chosen_text'} onClick={(e) => handleClick(e, 0, props.user, props.setActive)}>   Приостановлена</span>
+            <span className={props.user.status === 1 ? '' :'not_chosen_text'} onClick={(e) => handleClick(e, 1, props.user, props.setActive)}>   Подписка активна</span>
+            <span className={props.user.status === 2 ? '' :'not_chosen_text'} onClick={(e) => handleClick(e, 2, props.user, props.setActive)}>   Заблокирован</span>
         </div>
     );
 };

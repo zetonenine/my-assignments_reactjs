1ca02680d65e1797f5ea7d1188a7aad1e504ff59
Andrey <66561503+zetonenine@users.noreply.github.com>
2021-04-03 22:23:24 +0300

    Removed states

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,34 +1,26 @@
-import React, { useState } from 'react';
+import React from 'react';
 import { NavLink } from 'react-router-dom';
 import './Header.css';
 
 export default function Header() {
 
-    const [isActiveAll, setActiveAll] = useState(false);
-    const [isActiveBlocked, setActiveBlocked] = useState(false);
-    const [isActiveActive, setActiveActive] = useState(false);
-    
-    const toggleClass = (state) => {
-        state(true);
-    };
-
     return (
         <header className='titles'>
                 
             
-                <NavLink exact to="/" className="text" onClick={() => toggleClass(setActiveAll)}>
+                <NavLink exact to="/" className="text">
                     Все
-                    <div className={isActiveAll ? "square" : "square_off"}/>
+                    <div className="square"/>
                     
                 </NavLink>
-                <NavLink exact to='/blocked' className='text' onClick={() => toggleClass(setActiveBlocked)}>
+                <NavLink exact to='/blocked' className='text'>
                     Заблокированные
-                    <div className={isActiveBlocked ? "square" : "square_off"}/>    
+                    <div className="square"/>
                     
                 </NavLink>
-                <NavLink exact to='/active'  className='text' onClick={() => toggleClass(setActiveActive)} >
+                <NavLink exact to='/active'  className='text'>
                     Активные
-                    <div className={isActiveActive ? "square" : "square_off"}/>
+                    <div className="square"/>
                     
                 </NavLink>  
         </header>

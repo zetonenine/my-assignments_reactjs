d331d26553c83f8f4d7ce0ca28eaa288ab6d67e9
Andrey <66561503+zetonenine@users.noreply.github.com>
2021-04-03 21:20:58 +0300

    Removed ApiFetcher, put Api fetching into App.js. Added Router above Header

--- src/App.js
+++ src/App.js
@@ -1,20 +1,70 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import Header from './components/Header/Header';
-import MyComponent from './components/ContentBlock/Api/ApiFetcher'
+import All from './components/Pages/AllPage'
+import Blocked from './components/Pages/BlockedPage.js';
+import Active from './components/Pages/ActivePage.js';
 import './App.css';
-
+ 
 function App() {
+  const [error, setError] = useState(null);
+  const [isLoaded, setIsLoaded] = useState(false);
+  const [users, setUsers] = useState([]);
+ 
+
+  useEffect(() => {
+    fetch("https://watchlater.cloud.technokratos.com/get/array")
+    .then(res => res.json())
+    .then(
+      (result) => {
+        setIsLoaded(true);
+        setUsers(result);
+        },
+      (error) => {
+        setIsLoaded(true);
+        setError(error);
+      }
+    )
+  }, [])
+
 
   return (
     <div className='App'>
-      <div className='header'>
-        <Header/>
-      </div>
-      <MyComponent/>
+
+      <Router>     
+        <div className='header'>
+          <Header/>
+        </div>
+
+        <div>
+          {
+            error && <div>Error: {error.message}</div>
+          }
+          {
+            !isLoaded && <div className='loading_text'>Loading...</div>
+          }
+          {!error && isLoaded &&
+              <Switch>
+                <Route exact path='/'>
+                    <All users={users}/>
+                </Route>
+                <Route exact path='/active'>
+                    <Active users={users}/>
+                </Route>
+                <Route exact path='/blocked'>
+                    <Blocked users={users}/>
+                </Route>
+
+              </Switch>
+
+
+
+          }
+        </div>
+      </Router>
     </div>
-  );  
+
+  ); 
 }
-
+ 
 export default App;
-
-

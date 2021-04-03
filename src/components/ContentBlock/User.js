1ceba8147a9c733e2914288382359fcc09d1d033
Andrey <66561503+zetonenine@users.noreply.github.com>
2021-04-03 16:38:47 +0300

    Made a photo circle form

--- src/components/ContentBlock/User.js
+++ src/components/ContentBlock/User.js
@@ -14,10 +14,16 @@ export default function User(props) {
 
     return (
         <div key={props.data.id} className="users" onClick={() => props.handleClick(props.data)}> 
-            <div> 
-                <img className='image' src={props.data.avatar} alt=""></img>
-                <span className='name'>{props.data.fname} {props.data.name.split('')[0]}. {props.data.mname.split('')[0]}.</span>
-                <span className='balance'>Баланс: {Number(props.data.balance.toFixed(1)).toLocaleString('ru-RU').replace(',', '.')} </span>
+            <div>
+                <div >
+                    <div class="circular__landscape">
+                        <img className='image' src={props.data.avatar} alt=""></img>
+                    </div>
+                    
+                    <span className='name'>{props.data.fname} {props.data.name.split('')[0]}. {props.data.mname.split('')[0]}.</span>
+                    <span className='balance'>Баланс: {Number(props.data.balance.toFixed(1)).toLocaleString('ru-RU').replace(',', '.')} </span>
+                </div> 
+                
                 <p className='last_seen'>{Dater(props.data.lastUpdatedAt)}</p>
             </div>
             <div className='status' onClick={(e) => handleDropdownClick(e, props.data)}>

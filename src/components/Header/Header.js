09dbf322858e0b4043e2690e3c17b20d4f5e50ec
Andrey <66561503+zetonenine@users.noreply.github.com>
2021-04-03 16:03:13 +0300

    Put states into Headers for changing color and showing square. But it shows only for a moment while page is not refreshed

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,30 +1,34 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './Header.css';
 
 export default function Header() {
+
+    const [isActiveAll, setActiveAll] = useState(false);
+    const [isActiveBlocked, setActiveBlocked] = useState(false);
+    const [isActiveActive, setActiveActive] = useState(false);
+    
+    const toggleClass = (state) => {
+        state(true);
+    };
+
     return (
         
         <header className='titles'>
-            
             <nav>
-                <a className='text' href='/'>Все
-                    <div className="square"/>
-                    
+                <a className={isActiveAll ? 'text active' :'text'} href='/' onClick={() => toggleClass(setActiveAll)}>Все
+                    <div className={isActiveAll ? "square" : "square_off"}/>
                 </a>
             </nav>
             <nav>
-                <a className='text' href='/blocked'>Заблокированные
-                    <div className="square"/>
-                    
+                <a className={isActiveBlocked ? 'text active' :'text'} href='/blocked' onClick={() => toggleClass(setActiveBlocked)}>Заблокированные
+                    <div className={isActiveBlocked ? "square" : "square_off"}/>     
                 </a>
             </nav>
             <nav>
-                <a className='text' href='/active'>Активные
-                    <div className="square"/>
+                <a className={isActiveActive ? 'text active' :'text'} href='/active' onClick={() => toggleClass(setActiveActive)}>Активные
+                    <div className={isActiveActive ? "square" : "square_off"}/>
                 </a>
-            </nav>
-            
-             
+            </nav>      
         </header>
     )
 };
